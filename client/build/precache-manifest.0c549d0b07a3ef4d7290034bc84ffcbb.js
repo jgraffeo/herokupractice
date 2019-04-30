dbe99c9113498424f6166f71666b57ccdce65d83
jgraffeo <jgraffeo@gmail.com>
2019-04-29 20:42:37 -0700

    202 11 solved

---
+++ client/build/precache-manifest.0c549d0b07a3ef4d7290034bc84ffcbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afb9d1ed4f4796612b23",
+    "url": "/static/css/main.104cb802.chunk.css"
+  },
+  {
+    "revision": "afb9d1ed4f4796612b23",
+    "url": "/static/js/main.13daf7af.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "26553e7036fb655a5e00",
+    "url": "/static/js/2.f43e1651.chunk.js"
+  },
+  {
+    "revision": "4567404b585d08536c18e8115627b7bc",
+    "url": "/index.html"
+  }
+];
